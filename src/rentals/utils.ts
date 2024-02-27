ae12e3260848e597ad31cc5f989c76d05a95be15
0xcire <ciredeveloper@gmail.com>
2024-02-26 19:25:28 -0500

    feat: additional util functions wrapping dayjs

--- src/rentals/utils.ts
+++ src/rentals/utils.ts
@@ -1,5 +1,21 @@
 import dayjs from "../lib/day";
 
 export const isBetween = (target: Date, start: Date, end: Date): boolean => {
-  return dayjs(target).isBetween(dayjs(start), dayjs(end), "day", "[]");
+  return dayjs(target).isBetween(dayjs(start), dayjs(end), "hour", "[]");
+};
+
+export const dateIsInThePast = (comparison: Date): boolean => {
+  return dayjs(comparison).isBefore(dayjs(), "day");
+};
+
+export const dateIsInvalid = (date: Date): boolean => {
+  return date.toString() === "Invalid Date";
+};
+
+export const endIsBeforeStart = (start: Date, end: Date) => {
+  return dayjs(end).isBefore(start);
+};
+
+export const differenceInDays = (start: Date, end: Date) => {
+  return dayjs(end).diff(dayjs(start), "days");
 };
